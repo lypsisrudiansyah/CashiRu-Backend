cbf67c6ecfabb12c456cecf1a736dc5c32688325
rudiansyah <rudiansyahether@gmail.com>
2025-07-08 22:10:30 +0700

    ✨ (Order.php, OrderItem.php, migration): create OrderItem model and migration to store order items ✨ (Order.php): add relations to Order model for cashier and order items

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -9,4 +9,16 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Order extends Model
 {
     use HasFactory, SoftDeletes;
+
+    /* payment_method : cash, credit_card, etc. */
+
+    public function cashier()
+    {
+        return $this->belongsTo(User::class, 'cashier_id');
+    }
+
+    public function order_items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
