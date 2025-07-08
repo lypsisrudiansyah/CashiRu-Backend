e6feeac566b199e6151e62c2ce2ab1d35d328a40
rudiansyah <rudiansyahether@gmail.com>
2025-07-08 22:23:33 +0700

    ✨ (CategoryController.php): create CategoryController to handle category related API requests ✨ (api.php): add route for retrieving categories and login route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\AuthController;
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -19,6 +20,9 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [AuthController::class, 'login']);
+
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/categories', [CategoryController::class, 'index']);
 });
