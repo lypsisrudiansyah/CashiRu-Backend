8455e4ce825b506f2ab5d4fa5a5ae648f3c57efa
rudiansyah <rudiansyahether@gmail.com>
2025-07-08 16:11:24 +0700

    ✨ (Models): create Category model with soft deletes ✨ (Models): add soft deletes to User model ✨ (database/migrations): create cache and jobs tables ✨ (database/migrations): create categories table with soft deletes ♻️ (database/migrations): remove password_reset_tokens table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,12 +21,7 @@ return new class extends Migration
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
-        });
-
-        Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
-            $table->string('token');
-            $table->timestamp('created_at')->nullable();
+            $table->softDeletes();
         });
 
         Schema::create('sessions', function (Blueprint $table) {
@@ -45,7 +40,6 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('users');
-        Schema::dropIfExists('password_reset_tokens');
         Schema::dropIfExists('sessions');
     }
 };
