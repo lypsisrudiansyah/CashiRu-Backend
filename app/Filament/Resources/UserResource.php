31b91e76fa6a6ad9fae80818c864122520bda530
rudiansyah <rudiansyahether@gmail.com>
2025-07-09 21:09:47 +0700

    ✨ (UserResource.php, EditUser.php, ListUsers.php): create UserResource with form and table definitions; add pages for listing and editing users

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-user-group';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                TextInput::make('name')
+                    ->required()
+                    ->maxLength(70),
+                Forms\Components\Select::make('roles')
+                    ->options([
+                        'admin' => 'Admin',
+                        'user' => 'User',
+                        'kasir' => 'Kasir',
+                    ])
+                    ->preload()
+                    ->required(),
+                TextInput::make('email')
+                    ->email()
+                    ->required()
+                    ->maxLength(100),
+                TextInput::make('phone')
+                    ->tel()
+                    ->maxLength(15)
+                    ->default(null),
+                Forms\Components\TextInput::make('password')
+                    ->password()
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('name')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('email')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('phone')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('roles')
+                    ->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function beforeSave($record, $data): void
+    {
+        // if (!empty($data['password'])) {
+        //     $record->password = bcrypt($data['password']);
+        // } else {
+        //     // not replacing current password if its empty
+        //     unset($record->password);
+        // }
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+            'create' => Pages\CreateUser::route('/create'),
+            'edit' => Pages\EditUser::route('/{record}/edit'),
+        ];
+    }
+}
