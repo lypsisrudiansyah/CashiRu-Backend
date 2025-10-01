464f8db8e13aeacbb9b8eec51c99c227e6b3779d
rudiansyah <rudiansyahether@gmail.com>
2025-10-01 17:41:29 +0700

    feat: create OrderItemFactory.php

---
+++ database/factories/OrderItemFactory.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Order;
+use App\Models\OrderItem;
+use App\Models\Product;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\OrderItem>
+ */
+class OrderItemFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = OrderItem::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        $quantity = $this->faker->numberBetween(1, 5);
+        $price = $this->faker->randomFloat(2, 10, 200);
+
+        return [
+            'order_id' => Order::factory(),
+            'product_id' => Product::factory(),
+            'quantity' => $quantity,
+            'product_price' => $price,
+            'total_item' => $quantity * $price,
+        ];
+    }
+}
